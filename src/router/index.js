2497e319b8a1dbd85bbb820044963f3fa9d414f3
frankhn <harfrank2@gmail.com>
2019-07-25 19:51:41 +0200

    configure the routes for the imported components

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/views/Home'
+import Home from '@/views/Home'
+import About from '@/views/companyProfile/About'
+import VerifyEmail from '@/views/verifyEmail'
+import GetStarted from '@/views/sellers/GetStarted'
+import SingleProduct from '@/views/SingleProduct'
+import Profile from '@/views/profile/Index'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/verify',
+      name: 'verify Email',
+      component: VerifyEmail
+    },
+    {
+      path: '/getstarted',
+      name: 'verify Email',
+      component: GetStarted
+    },
+    {
+      path: '/deal/:slug',
+      name: 'product',
+      component: SingleProduct
+    },
+    {
+      path: '/profile/:username',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
